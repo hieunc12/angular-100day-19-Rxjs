4fbb4721713f96d52e1ae25df7337eb7d579b26b
hieunc12 <conghieu.ch@gmail.com>
2021-07-18 16:01:10 +0700

    3 files modified

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-import { of, map } from 'rxjs';
+import { fromEvent, throttleTime } from 'rxjs/dist/types';
 
-const source = of('World').pipe(map(x => `Hello ${x}!`));
+const rate = 1000;
+let lastMove = Date.now() - rate;
+document.addEventListener('mousemove', ev => {
+  if (Date.now() - lastMove >= rate) {
+    console.log(ev);
+    lastMove = Date.now();
+  }
+});
 
-source.subscribe(console.log);
+fromEvent(document, 'mousemove')
+  .pipe(throttleTime(1000))
+  .subscribe(console.log);
